416b9d08a20efc4e88df90e04ecf1b4aec2422fe
SentryPT <luis.peneirol@gmail.com>
2023-03-05 20:30:24 +0000

    exercicios

---
+++ cursoemvideo/Python 3 – Mundo 1/ex004.py
@@ -0,0 +1,6 @@
+inf = input('Escreva uma frase à sua escolha: ')
+print(f'O texto: {inf} é do tipo: ', type(inf))
+print(f'O texto: {inf} é alfanumerico: ', inf.isalnum())
+print(f'O texto: {inf} é espaço: ', inf.isspace())
+# é necessario colocar a variavel antes do is. No tipo a variavel vai para dentro
+
--- cursoemvideo/random_testes.py
+++ cursoemvideo/random_testes.py
@@ -1,11 +1,21 @@
 n1 = input('Digite um numero por favor: ')
 n2 = input('Digite novamente um numero por favor: ')
 s = float (n1) + float (n2)
-print(' O resultado é: ' , s)
-
 #indicar o tipo que se quer definir, antes da variavel e a variavel fica ()
+print (f'A soma entre {n1} e {n2} é igual a: {s}')
+print (n1.isalnum)
+
+# usamos as {} para selecionar uma variavel. apos isso fazemos .format para as chamar por ordem
+# f é mais simples para chamar a variavel.
 
 
-horapt = input('Indique a hora em Portugal: ')
+
+horapt = int (input('Indique a hora em Portugal: '))
 horaes = int (horapt) + 1
+print (type(horapt))
 print('A hora em Espanha é: ' , horaes)
+
+
+
+x= 'mariana'
+print (type (x))
