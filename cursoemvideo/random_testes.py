4b78f8db1f7dea45d635585d069c601ffd65c840
SentryPT <luis.peneirol@gmail.com>
2023-03-05 18:36:48 +0000

    horas

--- cursoemvideo/random_testes.py
+++ cursoemvideo/random_testes.py
@@ -4,3 +4,8 @@ s = float (n1) + float (n2)
 print(' O resultado é: ' , s)
 
 #indicar o tipo que se quer definir, antes da variavel e a variavel fica ()
+
+
+horapt = input('Indique a hora em Portugal: ')
+horaes = int (horapt) + 1
+print('A hora em Espanha é: ' , horaes)
