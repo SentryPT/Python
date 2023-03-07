7dfa7fd3d3e380e5b6956c99fc697ff36265cdac
Sentrypt <luis.peneirol@gmail.com>
2023-03-07 23:51:09 +0000

    opçao

--- cursoemvideo/Python 3 – Mundo 1/ex005.py
+++ cursoemvideo/Python 3 – Mundo 1/ex005.py
@@ -2,3 +2,8 @@ n = int (input('Indique um numero à sua escolha: '))
 preresult = n - 1
 posresult = n + 1
 print(f'O resultado é: {n} e fica entre {preresult} e {posresult}')
+
+#ou pode ser a seguinte opção:
+
+n = int (input ('Indique um valor à sua escolha: '))
+print (f'O valor indicado é {n} e o antecessor é {n -1} e o sucessor é {n + 1}')
