5e4524a71af12f5766d5b940b86436ea9ef04a7e
SentryPT <100774967+SentryPT@users.noreply.github.com>
2023-03-08 15:45:57 +0000

    exercicios

--- cursoemvideo/random_testes.py
+++ cursoemvideo/random_testes.py
@@ -29,4 +29,27 @@ print(f'A raiz quadrada do número é: {n**(1/2)}')
 print(f'O dobro do número é: {n*2}')
 print(f'O triplo do número é: {n*3}')
 
+print('Este programa irá converter um valor em metros e converter em cm e mm')
+m = float(input('Insira o valor em metros: '))
+print(f'O Valor em metros é: {m}, em cm é: {m * 100} e em mm é: {m * 1000}')
+
+n = int(input('Insira um número para mostrar a sua tabuada:'))
+print(f'1 x {n} é = a {n * 1}')
+print(f'2 x {n} é = a {n * 2}')
+print(f'3 x {n} é = a {n * 3}')
+print(f'4 x {n} é = a {n * 4}')
+print(f'5 x {n} é = a {n * 5}')
+print(f'6 x {n} é = a {n * 6}')
+print(f'7 x {n} é = a {n * 7}')
+print(f'8 x {n} é = a {n * 8}')
+print(f'9 x {n} é = a {n * 9}')
+print(f'10 x {n} é = a {n * 10}')
+
+
+print('- Cálculo de area de parede para pintura -')
+alt = float(input('Indique a altura da parede: '))
+larg = float( input('Indique a largura da parede:'))
+area= larg*alt
+print(f' Para pintar a seguinte área {area} m2, são necessários {area/2} l de tinta ')
+
 
