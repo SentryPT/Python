dc81021cd46d1f41ab7684d53dbd7a5dee1a87f5
Sentrypt <luis.peneirol@gmail.com>
2023-03-09 18:54:39 +0000

    Good ex12 e 13

--- cursoemvideo/Python 3 – Mundo 1/ex012.py
+++ cursoemvideo/Python 3 – Mundo 1/ex012.py
@@ -1,1 +1,5 @@
 #exercicio de calcular descontos
+print(' - Neste exercicio vamos calcular descontos de 20% em produtos - ')
+valor = float(input('Indique o valor do produto: '))
+desc = valor - valor * 0.2
+print(f'O produto que custava {valor} €, com o desconto, fica em {desc} €')
--- cursoemvideo/Python 3 – Mundo 1/ex013.py
+++ cursoemvideo/Python 3 – Mundo 1/ex013.py
@@ -1,1 +1,5 @@
-#exercicio de conversor de moedas
+#exercicio de algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+print('Vamos calcular um aumento salarial na ordem dos 15%')
+salario = float(input('Coloque o seu salario: €'))
+aumento = salario + salario * 0.15
+print(f'O seu salario de {salario} €, com o aumento de 15%, fica em {aumento} €')
