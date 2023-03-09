637dc898f416aabc22cc1f05a598b44fc525c2d4
Sentrypt <luis.peneirol@gmail.com>
2023-03-09 19:23:12 +0000

    ex014feito

--- cursoemvideo/Python 3 – Mundo 1/ex014.py
+++ cursoemvideo/Python 3 – Mundo 1/ex014.py
@@ -1,1 +1,5 @@
 #exercicio de conversor de temperaturas
+print('Neste exercicio vamos converter a temperatura em Cº para Fº')
+c = float(input('Insira a temperatura em graus Celsius: º'))
+f = c * 1.8 + 32
+print(f'A temperatura registada com {c} ºC equivale a {f} ºF')
