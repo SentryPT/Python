8e94c2bbd4cfb14d75f915af7c94d0f013f71ee9
Sentrypt <luis.peneirol@gmail.com>
2023-03-09 00:00:12 +0000

    ex11

--- cursoemvideo/Python 3 – Mundo 1/ex011.py
+++ cursoemvideo/Python 3 – Mundo 1/ex011.py
@@ -1,2 +1,6 @@
 #exercicio de pintar a parede
 print('Quantos litros de tinta vamos precisar para pintar determinada área?')
+a=float(input('Insira o valor da altura '))
+l=float(input('Insira o valor da largura: '))
+area=a*l
+print(f'Para a seguinte área {area}, são necessários {area/2} litros de tinta.  ')
