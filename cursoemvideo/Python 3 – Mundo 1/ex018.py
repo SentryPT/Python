7fe45b41f20365e624c489bae535dc53328833e9
Sentrypt <luis.peneirol@gmail.com>
2023-03-14 22:34:03 +0000

    sorteio_lista

--- cursoemvideo/Python 3 – Mundo 1/ex018.py
+++ cursoemvideo/Python 3 – Mundo 1/ex018.py
@@ -1,4 +1,5 @@
-#exercicio de Seno, Cosseno e Tangente
+#exercicio de Seno, Cosseno e Tangente!
+
 #Faça um programa que leia um ângulo e mostre o valor de seno, cosseno e tangente...
 import math
 ang = float(input('Indique um ângulo à sua escolha: '))
--- cursoemvideo/Python 3 – Mundo 1/ex019.py
+++ cursoemvideo/Python 3 – Mundo 1/ex019.py
@@ -4,3 +4,14 @@ alunos = ['André' , 'Mara' , 'Luis' , 'Josué']
 print(random.choice(alunos))
 
 
+#Faz uma lista de ordenação de apresentação:
+import random
+n1 = str(input('Indique um aluno: '))
+n2 = str(input('Indique o segundo aluno: '))
+n3 = str(input('Indique o terceiro aluno: '))
+n4 = str(input('Indique o quarto aluno: '))
+
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem é a seguinte: ')
+print(lista)
