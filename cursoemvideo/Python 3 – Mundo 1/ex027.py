e6806c4212287ab9428f8139245ea672234cb747
Sentrypt <luis.peneirol@gmail.com>
2023-03-18 00:21:05 +0000

    ex27

--- cursoemvideo/Python 3 – Mundo 1/ex027.py
+++ cursoemvideo/Python 3 – Mundo 1/ex027.py
@@ -1,5 +1,5 @@
 #programa que lê o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 nome = str (input('Indique o seu nome completo: ')).strip()
 n = nome.split()
-print(f"O seu primeiro nome é: { n [0] } e o seu apelido é {n [-1]}") 
+print(f"O seu primeiro nome é: { n [0] } e o seu apelido é { n [-1] } ") 
 #o [-1] pode ser utilizado para se referir ao último objeto de uma lista, assim como [-2] seria a penúltima e assim por diante
