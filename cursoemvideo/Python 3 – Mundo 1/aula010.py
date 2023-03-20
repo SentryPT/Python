5b4059ecc16fab6aef130d9e88c4112faa517604
SentryPT <100774967+SentryPT@users.noreply.github.com>
2023-03-20 15:57:30 +0000

    Update aula010.py

--- cursoemvideo/Python 3 – Mundo 1/aula010.py
+++ cursoemvideo/Python 3 – Mundo 1/aula010.py
@@ -21,3 +21,68 @@ if m >9.5:
     print(f'Felizmente a nota de {m :.3} deu para ter positiva')
 else:
     print(f'Infelizmente com a nota de {m :.3} és um burro do crl')
+    
+
+#ex 28
+num = int (input('Escreva um número entre 0 e 5: '))
+if num > 5:
+    print(f'O valor que indicou, {num}, está errado. Tente novamente')
+else:
+    print(f'Acertou. {num} tinha sido o valor que eu tinha pensado.')
+    
+#ex28 resolução
+from random import randint
+pc = randint(0, 5) #cria num aleatorio
+print('Irei pensar num valor')
+jogador = int(input('O número que vou indicar é o seguinte: '))
+if jogador == pc:
+    print(f'Acertou no numero {pc}. Muitos parabéns!')
+else:
+    print(f'Já foste. Grande abraço')
+
+
+#Ex29
+vel = float (input('Indique a velocidade a que ia no carro:'))
+if vel > 80:
+    print(f'Foi apanhado a {vel}km/h. Foi multado por excesso de velocidade. ')
+    multa = (vel - 80) * 7
+    print(f' Terá de pagar uma multa no valor de {multa}€')
+else:
+    print(f'À velocidade a que vai, {vel}km/h, está dentro do limite legal.')
+print('Conduza com segurança')
+
+#Exercicio da taxa de alcool
+
+print('-=-'*20)
+print('Cálculo de taxa de alcool para saber se terá contra-ordenação')
+print('-=-'*20)
+tx_alcool = float ( input(' Indique a taxa de alcool no sangue: '))
+if tx_alcool <= 0.49:
+    print(f'Pois bem. Com uma taxa de {tx_alcool}, és um PedroGF45')
+elif 0.5 >= tx_alcool and tx_alcool <= 0.79: 
+    print(f'Pois é meu menino. Já levas de prémio entre 250€ a 1250€')
+elif 0.8 >= tx_alcool and tx_alcool <= 1.2:
+    print(f'A tua taxa de {tx_alcool}, é à Peneirol. Larga os 500€ até 2500€.')
+elif tx_alcool >= 1.2:
+    print(f'Oh maninho, com essa taxa de {tx_alcool}, já vais é de cana. Aquele abraço.')
+print('Se conduzir, não beba')
+print('-=-'*20)
+
+
+altura = float (input('Indique a sua altura, em m:'))
+peso = float (input('Indique o seu peso, em kg: '))
+imc = peso / (altura * altura)
+if imc <= 18.49:
+    print(f'O seu imc de {imc :.2}, está no nivel de Magreza')
+elif imc >= 18.5 and imc <= 24.9:
+    print(f'O seu imc de {imc :.2}, está no nível de Normal')
+elif imc >= 25 and imc <= 29.9:
+    print(f'O seu imc de {imc :.2}, está no nível de Sobrepeso')
+elif imc >= 30 and imc <= 34.9:
+    print(f'O seu imc de {imc :.2}, está no nível de Obesidade Grau I')
+elif imc >= 35 and imc <= 39.9:
+    print(f'O seu imc de {imc :.2}, está no nível de Obesidade Grau II')
+else:
+    print(f'O seu imc de {imc :.2}, está no nível de Obesidade Grau III')
+
+print('Promova um hábito de vida saudável, ingira acima de 2l de água por dia e realize atividade física diariamente. Por si, pela sua saúde.')
