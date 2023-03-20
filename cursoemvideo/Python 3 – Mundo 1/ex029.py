75e4d504f39df96508b828b73f8c17dafb394054
SentryPT <luis.peneirol@gmail.com>
2023-03-20 22:49:57 +0000

    ex29

---
+++ cursoemvideo/Python 3 – Mundo 1/ex029.py
@@ -0,0 +1,6 @@
+#ex 29 par ou impar
+num = int (input ('Digite um numero, para ver se é par ou impar'))
+if num % 2 == 0:
+    print(f'O valor indicado, {num}, é par. ')
+else:
+    print(f'O valor indicado, {num}, é impar.')
